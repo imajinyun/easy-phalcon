959b1d59710325cb960f901bbeb2f3fac49b59e5
imajinyun <imajinyun@gmail.com>
2018-03-28 21:02:43 +0800

    : ) Add cache.php file.

---
+++ config/cache.default.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Cache Store
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default cache connection that gets used while
+    | using this caching library. This connection is used when another is
+    | not explicitly specified when executing a given caching function.
+    |
+    */
+    'default'     => env('CACHE_DRIVER', 'redis'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define all of the cache "stores" for your application as
+    | well as their drivers. You may even define multiple stores for the
+    | same cache driver to group types of items stored in your caches.
+    |
+    */
+    'connections' => [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Redis Connection
+        |--------------------------------------------------------------------------
+        |
+        |
+        */
+        'redis' => [
+            'driver'  => 'cluster',
+            'default' => [
+                'backend'    => \Phalcon\Cache\Backend\Redis::class,
+                'frontend'   => \Phalcon\Cache\Frontend\Data::class,
+                'connection' => env('CACHE_REDIS_CONNECTION', 'default'),
+                'lifetime'   => 172800,
+                'options'    => [
+                    'host'       => '127.0.0.1',
+                    'port'       => 6379,
+                    'auth'       => '',
+                    'index'      => 0,
+                    'prefix'     => '',
+                    'persistent' => false,
+                ],
+            ],
+            'cluster' => [
+                'client'  => \Predis\Client::class,
+                'servers' => [
+                    'tcp://127.0.0.1:6381',
+                    'tcp://127.0.0.2:6382',
+                    'tcp://127.0.0.2:6383',
+                ],
+                'options' => [
+                    'prefix'   => '',
+                    'password' => '',
+                ],
+            ],
+        ],
+    ],
+];
