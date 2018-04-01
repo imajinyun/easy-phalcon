67ee9938fede671d1e17faf0c5d4611e8715f1cf
imajinyun <imajinyun@gmail.com>
2018-04-01 21:27:37 +0800

    : ) Update UserAccountException class.

--- app/Exception/UserAccountException.php
+++ app/Exception/UserAccountException.php
@@ -16,14 +16,14 @@ class UserAccountException extends \LogicException
      *
      * @param string          $message
      * @param int             $statusCode
-     * @param \Throwable|null $previous
      * @param int             $code
+     * @param \Throwable|null $previous
      */
     public function __construct(
         string $message = '',
         int $statusCode = 0,
-        Throwable $previous = null,
-        int $code = 0
+        int $code = 0,
+        Throwable $previous = null
     ) {
         $this->statusCode = $statusCode;
         parent::__construct($message, $code, $previous);
